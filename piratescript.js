f268eb3335809d0e170cefce52395298fd5d3ea9
KaisenFletcher <mbiggs2000@outlook.com>
2022-05-17 20:01:08 +0100

    1.3
    works...

--- piratescript.js
+++ piratescript.js
@@ -3,24 +3,10 @@ function piratize(input) {
 
     const arr = input.split(' ')
   
-    return arr
-   
-
-      .map((word) => {
-        return word.match(/[A-z]/i)
-          ? `${word.substr(1)}${word.substr(0, 1)}arr`
-          : word
-
-        
-          if(arr = "oundsparr"){
-          return text.replace("oundsparr", "doubloons")}
-          // cant get this bit to work
-
-      })
-
-      .join(' ')
-
-      
+    return arr.map((word) => {
+      if (word === "pounds") return "doubloons"
+      return word.match(/[A-z]/i) ? `${word.substr(1)}${word.substr(0, 1)}arr` : word
+    }).join(' ');
 
   }
   
