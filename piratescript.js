09bddb1cfb609226401d4d196e33019978b3cdae
KaisenFletcher <mbiggs2000@outlook.com>
2022-05-17 14:35:52 +0100

    1.1
    cant get doubloons to work

--- piratescript.js
+++ piratescript.js
@@ -2,12 +2,20 @@
 function piratize(input) {
 
     const arr = input.split(' ')
+  
     return arr
+   
+
       .map((word) => {
         return word.match(/[A-z]/i)
           ? `${word.substr(1)}${word.substr(0, 1)}arr`
           : word
-       
+
+        
+          if(arr = "oundsparr"){
+          return text.replace("oundsparr", "doubloons")}
+          // cant get this bit to work
+
       })
 
       .join(' ')
